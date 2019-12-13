562702f4ef53250a6aa32a1257b84b10710830f3
yao000 <1024107565@qq.com>
2019-12-13 20:11:40 +0800

    1.shuyao test

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
-public void main (String []){
-    System.out.print("aaaaaaaa");
-        }
+public class hello {
+    public static void main(String[] args) {
+        System.out.print("test");
+    }
+}
